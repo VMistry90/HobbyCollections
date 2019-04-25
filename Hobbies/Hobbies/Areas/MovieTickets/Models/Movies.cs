5327bde42c6f63e72662130ea431bdd6761cbbe2
VMistry90 <48362565+VMistry90@users.noreply.github.com>
2019-04-25 17:09:46 -0400

    Added Movie Tickets 1 and 2

---
+++ Hobbies/Hobbies/Areas/MovieTickets/Models/Movies.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hobbies.Areas.MovieTickets.Models
+{
+    public class Movies
+    {
+        public string IndianaJones { get; set; }
+
+        public string QuantumofSolace { get; set; }
+
+        public string DaytheEarthStoodStill { get; set; }
+
+        public string XmenOrigins_Wolvervine { get; set; }
+
+        public string StarTrek { get; set; }
+
+        public string SlumDogMillionaire { get; set; }
+
+        public string District9 { get; set; }
+
+        public string Kites { get; set; }
+
+        public string Noah { get; set; }
+
+        public string Avengers_InfinityWar { get ;set; }
+
+        public string Anabelle { get; set; }
+
+        public string JungleBook { get; set; }
+
+        public string Skyfall { get; set; }
+
+        public string Spectre { get; set; }
+
+        public string StarWars { get; set; }
+
+        public string BajiraoMastani { get; set; }
+
+        public string FinestHours { get; set; }
+
+        public string Deadpool { get; set; }
+
+        public string LondonHasFallen { get; set; }
+
+        public string CaptainAmerica { get; set; }
+
+        public string MissionImpossible2016 { get; set; }
+
+        public string Startrek_2016 { get; set; }
+
+        public string HungerGames_Mocking1 { get; set; }
+
+        public string RedRidingHood { get; set; }
+
+        public string BattleLA { get; set; }
+
+        public string PiratesofCaribean { get; set; }
+
+        public string Priest { get; set; }
+
+        public string LionKing3D { get; set; }
+
+        public string GreenLantern { get; set; }
+
+        public string Immortals { get; set; }
+
+        public string MissionImpossible_2013 { get; set; }
+
+        public string HungerGames_2012 { get; set; }
+
+        public string Avengers_2012 { get; set; }
+
+    }
+}
---
+++ Hobbies/Hobbies/Areas/MovieTickets/MovieTicketsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Hobbies.Areas.MovieTickets
+{
+    public class MovieTicketsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "MovieTickets";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "MovieTickets_default",
+                "MovieTickets/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
